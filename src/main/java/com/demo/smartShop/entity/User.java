569c5ae345b1eefecc3a3a1091ce991914df4515
Kawtar <Kawtar.shaimi8@gmail.com>
2025-11-24 16:55:01 +0100

    SMAR-47 : Ajout de l'entité User

---
+++ src/main/java/com/demo/smartShop/entity/User.java
@@ -0,0 +1,27 @@
+package com.demo.smartShop.entity;
+
+import com.demo.smartShop.entity.enums.UserRole;
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Table(name = "users")
+@Data
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    private UserRole role;
+
+    @OneToOne(mappedBy = "user")
+    private Client client;
+
+}
