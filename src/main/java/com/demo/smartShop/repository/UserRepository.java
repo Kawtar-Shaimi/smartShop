b500aff2b2a7f7ae81b0e33349c825f1aee3c21a
Kawtar <Kawtar.shaimi8@gmail.com>
2025-11-25 16:52:59 +0100

    SMAR-48 : Ajout le repository de user

---
+++ src/main/java/com/demo/smartShop/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.demo.smartShop.repository;
+
+import com.demo.smartShop.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByUsername(String username);
+    boolean existsByUsername(String username);
+}
