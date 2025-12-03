979a5418dbb312694fad22ef0386686edb786e34
Kawtar <Kawtar.shaimi8@gmail.com>
2025-12-03 20:14:09 +0100

    SMAR-4 : Modification de repository user.

--- src/main/java/com/demo/smartShop/repository/UserRepository.java
+++ src/main/java/com/demo/smartShop/repository/UserRepository.java
@@ -2,9 +2,11 @@ package com.demo.smartShop.repository;
 
 import com.demo.smartShop.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
 import java.util.Optional;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByUsername(String username);
-    boolean existsByUsername(String username);
 }
