4a42c68b6be0d704e287b5e7a99136fa52db0353
Kawtar <Kawtar.shaimi8@gmail.com>
2025-12-04 17:27:18 +0100

    SMAR-35 : Ajout de lPaymentDTO.

---
+++ src/main/java/com/demo/smartShop/dto/PaymentDTO.java
@@ -0,0 +1,38 @@
+package com.demo.smartShop.dto;
+
+import com.demo.smartShop.entity.enums.PaymentStatus;
+import com.demo.smartShop.entity.enums.PaymentType;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class PaymentDTO {
+    private Long id;
+
+    @javax.validation.constraints.NotNull(message = "Order ID is required")
+    private Long orderId;
+
+    private int paymentNumber;
+
+    @javax.validation.constraints.NotNull(message = "Amount is required")
+    @javax.validation.constraints.Positive(message = "Amount must be positive")
+    private BigDecimal amount;
+
+    @javax.validation.constraints.NotNull(message = "Payment type is required")
+    private PaymentType type;
+
+    private PaymentStatus status;
+    private LocalDateTime paymentDate;
+    private LocalDateTime cashingDate;
+    private String reference;
+    private String bank;
+    private LocalDateTime dueDate;
+}
