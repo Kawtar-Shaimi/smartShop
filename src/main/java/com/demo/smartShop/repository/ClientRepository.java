b515b2f666030b614100d46761020cad1458a443
Kawtar <Kawtar.shaimi8@gmail.com>
2025-12-04 15:28:22 +0100

    SMAR-9 : Ajout de ClientRepository.

---
+++ src/main/java/com/demo/smartShop/repository/ClientRepository.java
@@ -0,0 +1,12 @@
+package com.demo.smartShop.repository;
+
+import com.demo.smartShop.entity.Client;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface ClientRepository extends JpaRepository<Client, Long> {
+    Optional<Client> findByEmail(String email);
+}
