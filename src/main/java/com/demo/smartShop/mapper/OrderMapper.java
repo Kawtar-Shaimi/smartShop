f6cefe3d6b0dfa09bd2470127105ceaa3bf5cf16
Kawtar <Kawtar.shaimi8@gmail.com>
2025-12-04 17:12:13 +0100

    SMAR-22 : Ajout de OrderMapper.

---
+++ src/main/java/com/demo/smartShop/mapper/OrderMapper.java
@@ -0,0 +1,26 @@
+package com.demo.smartShop.mapper;
+
+import com.demo.smartShop.dto.OrderDTO;
+import com.demo.smartShop.dto.OrderItemDTO;
+import com.demo.smartShop.entity.Order;
+import com.demo.smartShop.entity.OrderItem;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+@Mapper(componentModel = "spring")
+public interface OrderMapper {
+
+    @Mapping(source = "client.id", target = "clientId")
+    @Mapping(source = "client.nom", target = "clientName")
+    OrderDTO toDTO(Order order);
+
+    @Mapping(source = "clientId", target = "client.id")
+    Order toEntity(OrderDTO orderDTO);
+
+    @Mapping(source = "product.id", target = "productId")
+    @Mapping(source = "product.nom", target = "productName")
+    OrderItemDTO toDTO(OrderItem orderItem);
+
+    @Mapping(source = "productId", target = "product.id")
+    OrderItem toEntity(OrderItemDTO orderItemDTO);
+}
