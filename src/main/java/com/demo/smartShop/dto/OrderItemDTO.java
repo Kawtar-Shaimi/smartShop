80fa7be2efbdd917b61fa279a4f420d330276529
Kawtar <Kawtar.shaimi8@gmail.com>
2025-12-04 17:10:25 +0100

    SMAR-22 : Ajout de OrderItemDTO.

---
+++ src/main/java/com/demo/smartShop/dto/OrderItemDTO.java
@@ -0,0 +1,21 @@
+package com.demo.smartShop.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class OrderItemDTO {
+    private Long id;
+    private Long productId;
+    private String productName;
+    private int quantity;
+    private BigDecimal unitPrice;
+    private BigDecimal totalLine;
+}
