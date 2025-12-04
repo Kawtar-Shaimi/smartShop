fefff983b5b252e6a98985114df358e8d387e9aa
Kawtar <Kawtar.shaimi8@gmail.com>
2025-12-04 17:29:52 +0100

    SMAR-35 : Ajout de PaymetMapper.

---
+++ src/main/java/com/demo/smartShop/mapper/PaymentMapper.java
@@ -0,0 +1,15 @@
+package com.microtech.smartshop.mapper;
+
+import com.demo.smartShop.dto.PaymentDTO;
+import com.demo.smartShop.entity.Payment;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+@Mapper(componentModel = "spring")
+public interface PaymentMapper {
+    @Mapping(source = "order.id", target = "orderId")
+    PaymentDTO toDTO(Payment payment);
+
+    @Mapping(source = "orderId", target = "order.id")
+    Payment toEntity(PaymentDTO paymentDTO);
+}
