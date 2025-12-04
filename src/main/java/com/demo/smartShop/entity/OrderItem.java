6c2a2846a9d204d771d1595df86675c30c1f9834
Kawtar <Kawtar.shaimi8@gmail.com>
2025-12-04 17:08:37 +0100

    SMAR-22 : Ajout de l'entité OrderItem.

---
+++ src/main/java/com/demo/smartShop/entity/OrderItem.java
@@ -0,0 +1,39 @@
+package com.demo.smartShop.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+
+@Entity
+@Table(name = "order_items")
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class OrderItem {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "order_id", nullable = false)
+    private Order order;
+
+    @ManyToOne
+    @JoinColumn(name = "product_id", nullable = false)
+    private Product product;
+
+    @Column(nullable = false)
+    private int quantity;
+
+    @Column(name = "unit_price", nullable = false)
+    private BigDecimal unitPrice;
+
+    @Column(name = "total_line", nullable = false)
+    private BigDecimal totalLine;
+}
